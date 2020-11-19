b160e137f0cf8228b0f29b1c63319a73253b3299
Olesya Steinmetz <newsabome@icloud.com>
2020-11-19 13:53:44 +0100

    first commit

--- src/model/ShoppingCart.js
+++ src/model/ShoppingCart.js
@@ -18,23 +18,39 @@ export default class ShoppingCart {
     checkout = () => {
         let totalPrice = 0;
         let loyaltyPointsEarned = 0;
+        let amountOfProducts = 0;
+        let bulkProducts = []
 
         this.products.forEach(product => {
             let discount = 0;
-            if (product.code.startsWith("DIS_10")) {
-                discount = product.price * 0.1;
-                loyaltyPointsEarned += product.price / 10;
-            } else if (product.code.startsWith("DIS_15")) {
-                discount = product.price * 0.15;
-                loyaltyPointsEarned += product.price / 15;
+            if (product.code.startsWith("BULK_BUY_2_GET_1")) {
+                bulkProducts.push(product);
+                
             } else {
-                loyaltyPointsEarned += product.price / 5;
+                if (product.code.startsWith("DIS_10")) {
+                    discount = product.price * 0.1;
+                    loyaltyPointsEarned += product.price / 10;
+                } else if (product.code.startsWith("DIS_15")) {
+                    discount = product.price * 0.15;
+                    loyaltyPointsEarned += product.price / 15;
+                } else {
+                    loyaltyPointsEarned += product.price / 5;
+                }
+    
+                totalPrice += product.price - discount;
             }
-
-            totalPrice += product.price - discount;
         });
 
+        amountOfProducts = Math.ceil(bulkProducts.length / 3 * 2);
+        console.log('>>>>> ', bulkProducts.indexOf(0));
+        totalPrice += amountOfProducts * bulkProducts[0].price;
+
+        if(totalPrice >= 500){
+            totalPrice = totalPrice * 0.95;
+        }
+
         return new Order(totalPrice, loyaltyPointsEarned);
+    
     };
 
     displaySummary = () =>  {
--- test/model/ShoppingCart.spec.js
+++ test/model/ShoppingCart.spec.js
@@ -5,12 +5,15 @@ import ShoppingCart from "../../src/model/ShoppingCart.js";
 describe("Shopping cart should checkout", () => {
 
     it("Should calculate correct total and loyalty points for 10% discounted products", () => {
+        // given
         const customer = new Customer("Test customer");
         const products = [new Product(100, "DIS_10_TestProduct", "Test product")];
         const shoppingCart = new ShoppingCart(customer, products);
         
+        // when
         const order = shoppingCart.checkout();
 
+        // then
         expect(order.totalPrice).toBe(90);
         expect(order.loyaltyPoints).toBe(10);
     });
@@ -35,6 +38,43 @@ describe("Shopping cart should checkout", () => {
 
         expect(order.totalPrice).toBe(100);
         expect(order.loyaltyPoints).toBe(20);
+    });
+
+    it("Should calculate a 5% discount on total purchase above $500", () => {
+        // given
+        const customer = new Customer('Olesya')
+        const products = [
+            new Product(200, "TestProduct1", "Test product1"),
+            new Product(300, "TestProduct2", "Test product2")
+        ];
+        const shoppingCart = new ShoppingCart(customer, products);
+        // when
+        const order = shoppingCart.checkout();
+        //const order = {
+        //    totalPrice: 500,
+         //   loyaltyPointsEarned: 50
+       // }
+        // then
+        expect(order.totalPrice).toBe(475);
+    });
+
+    it("Should get third product for free if two products have bulk code", () => {
+        // given
+        const customer = new Customer('Olesya')
+        const products = [
+            new Product(200, "BULK_BUY_2_GET_1Product1", "Test product1"),
+            new Product(200, "BULK_BUY_2_GET_1Product2", "Test product2"),
+            new Product(200, "BULK_BUY_2_GET_1Product2", "Test product2")
+        ];
+        const shoppingCart = new ShoppingCart(customer, products);
+        // when
+        const order = shoppingCart.checkout();
+        //const order = {
+        //    totalPrice: 600,
+         //   loyaltyPointsEarned: 50
+       // }
+        // then
+        expect(order.totalPrice).toBe(400);
     });
 });
 
